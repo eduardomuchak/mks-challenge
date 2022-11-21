9893a7488fd05a13e6d0f285d18a3e653b1f65da
Eduardo Muchak <eduardomuchak@gmail.com>
2022-11-21 20:02:35 -0300

    style: :lipstick: adiciona componente loading wrapper

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import toast, { Toaster } from 'react-hot-toast';
 
 import { Jelly } from '@uiball/loaders';
 
+import { LoadingWrapper } from '../../components/LoadingWrapper/index.style';
 import { useGetProductQuery } from '../../services/apiSlice';
 export const Home = () => {
   const { isLoading, isError } = useGetProductQuery({ page: 1, rows: 8, sort: 'id', order: 'ASC' });
@@ -11,12 +12,12 @@ export const Home = () => {
 
   if (isLoading) {
     return (
-      <>
+      <LoadingWrapper>
         <Jelly size={80} speed={0.9} color="#0F52BA" />
-      </>
+      </LoadingWrapper>
     );
   }
-  if (!isError) {
+  if (isError) {
     toast.error('Opa! Aconteceu algum erro ao buscar as informações', {
       id: 'toast-principal',
     });
